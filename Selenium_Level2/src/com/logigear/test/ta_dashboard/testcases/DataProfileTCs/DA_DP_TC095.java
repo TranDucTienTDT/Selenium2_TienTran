d4b17f7b5fba9dac25dbbd0dbbd8e366a47e5185
HanhNguyen0808 <48435186+HanhNguyen0808@users.noreply.github.com>
2019-07-02 13:59:33 +0700

    change name of TC 95

--- Selenium_Level2/src/com/logigear/test/ta_dashboard/testcases/DataProfileTCs/DA_DP_TC095.java
+++ Selenium_Level2/src/com/logigear/test/ta_dashboard/testcases/DataProfileTCs/DA_DP_TC095.java
@@ -5,15 +5,15 @@ import org.testng.annotations.Test;
 
 import com.logigear.test.ta_dashboard.data_object.DataProfile;
 import com.logigear.test.ta_dashboard.pom.DPGeneralSettingsPage;
+import com.logigear.test.ta_dashboard.pom.DPGeneralSettingsPage.TableNavigatedPage;
 import com.logigear.test.ta_dashboard.pom.DPStatisticFieldsPage;
 import com.logigear.test.ta_dashboard.pom.HomePage;
 import com.logigear.test.ta_dashboard.testcases.Precondition;
-import com.logigear.test.ta_dashboard.pom.DPGeneralSettingsPage.TableNavigatedPage;
 
 public class DA_DP_TC095 extends Precondition{
 	
 	@Test
-	public void DA_DP_TC095() {
+	public void TC_DA_DP_TC095() {
 
 		LOG.info("Verify that all fields of selected \"Item Type\" item are populated correctly ");
 		//init data
